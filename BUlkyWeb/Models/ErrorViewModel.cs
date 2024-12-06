5f4357eb6223fdef4d509b7b908ae274a5b36195
shahil <kamohamedshahil@gmail.com>
2024-12-06 12:00:49 +0530

    Add project files.

---
+++ BUlkyWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BUlkyWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
