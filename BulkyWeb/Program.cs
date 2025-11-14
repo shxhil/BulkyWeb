f9be149b83f77f2d4516192c46f479ea528c7f03
shxhil <kamohamedshahil@gmail.com>
2025-11-14 12:11:22 +0530

    ajax and js corrected

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,11 +1,11 @@
-using Bulky.DataAccess.Repository.IRepostory;
+﻿using Bulky.DataAccess.Repository.IRepostory;
 using Bulky.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Bulky.DataAccess.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//if we need to register somthing for our application itd is done in program.cs
+//if we need to register somthing for our application itd is done in program.cs after builder
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -13,9 +13,10 @@ builder.Services.AddControllersWithViews();
 //mixed combination of both sqlserver and sqlite
 
 var machineName = Environment.MachineName;
+bool useSqlite = machineName == "DESKTOP-M04RAKA";
 Console.WriteLine($"Running on machine: {machineName}");
 
-if (machineName == "DESKTOP-M04RAKA")
+if (useSqlite)
 {
     builder.Services.AddDbContext<ApplicationDbContext>(options =>
         options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -29,6 +30,22 @@ else
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
+
+
+// 2. 📌 Place DB initialization RIGHT HERE
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    if (useSqlite)
+    {
+        db.Database.EnsureCreated();   // Auto-create tables (SQLite)
+    }
+    else
+    {
+        db.Database.Migrate();         // Apply EF migrations (SQL Server)
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
--- BulkyWeb/wwwroot/js/product.js
+++ BulkyWeb/wwwroot/js/product.js
@@ -1,18 +1,38 @@
-﻿$(document).ready(function () {
-    loadTableData()
-})
-
+﻿
+$(document).ready(function () {
+    loadTableData();
+});
 
 function loadTableData() {
-    $('#tblData').DataTable({
-        "ajax": {url: '/admin/product/getall'}
-    },
+    new DataTable('#tblData', {
+        "ajax": {
+            "url": '/Admin/Product/GetAll',
+            "dataSrc": 'data' // This matches the { data = productObj } from your controller
+        },
         "columns": [
-            { data: 'Title' , "width":"15%"},
-            { data: 'ISBN', "width": "15%" },
-            { data: 'ListPrice', "width": "15%" },
-            { data: 'Author', "width": "15%" }
-            { data: 'Category.Name', "width": "15%" }
-    ]
-    );
+            { data: 'title', width: "15%" },
+            { data: 'isbn', width: "15%" },
+            { data: 'listPrice', "render": data => `$${data.toFixed(2)}`, width: "10%" }, // Formats price
+            { data: 'author', width: "15%" },
+            { data: 'category.name', width: "15%" },
+            {
+                // This is the 6th column for the buttons
+                data: 'id', // Use the 'id' of the product for the links
+                "render": function (data) {
+                    // 'data' is the product's ID
+                    return `
+                        <div role="group" class="btn-group w-100">
+                            <a href="/Admin/Product/Upsert?id=${data}" class="btn btn-primary mx-2">
+                                <i class="bi bi-pencil-square"></i> Edit
+                            </a>
+                            <a href="/Admin/Product/Delete?id=${data}" class="btn btn-danger mx-2">
+                                <i class="bi bi-trash"></i> Delete
+                            </a>
+                        </div>
+                    `;
+                },
+                width: "20%"
+            }
+        ]
+    });
 }
