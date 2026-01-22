d0735229dc5d5660d887b268fc18e9119cd7c3c3
shahil <kamohamedshahil@gmail.com>
2026-01-22 23:21:31 +0530

    Authorization in Project

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -37,6 +37,13 @@ else
 }
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+
 builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
