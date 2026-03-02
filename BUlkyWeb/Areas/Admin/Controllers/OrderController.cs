8ee7033ff05e4713800b7524fafbddaea0e26476
Shahil <Shahil@DESKTOP-M04RAKA>
2026-03-02 18:06:55 +0530

    Order Processing Buttons Logic => Partially js need to done

--- BUlkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BUlkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -2,15 +2,19 @@
 using Bulky.Models;
 using Bulky.Models.ViewModels;
 using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace BulkyWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        public OrderVM OrderVM { get; set; }
         public OrderController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
@@ -19,21 +23,93 @@ namespace BulkyWeb.Areas.Admin.Controllers
         {
             return View();
         }
+
         public IActionResult Details(int id)
         {
-            OrderVM orderVM = new()
+            OrderVM  = new()
             {
                 OrderHeader = _unitOfWork.OrderHeader.Get(x => x.Id == id, includeProperty: "ApplicationUser"),
                 OrderDetail=_unitOfWork.OrderDetail.GetAll(x=> x.OrderHeaderId == id, includeProperty:"product"),
             };
             
-            return View(orderVM);
+            return View(OrderVM);
+        }
+        [HttpPost]
+        [Authorize(Roles =SD.Role_Admin+","+SD.Role_Employee)]
+        public IActionResult UpdateOrderDetails(OrderVM orderVM)
+        {
+            var orderHeaderFromDB = _unitOfWork.OrderHeader
+                .Get(x => x.Id == orderVM.OrderHeader.Id);
+
+            orderHeaderFromDB.Name = orderVM.OrderHeader.Name;
+            orderHeaderFromDB.PhoneNumber = orderVM.OrderHeader.PhoneNumber;
+            orderHeaderFromDB.StreetAddress = orderVM.OrderHeader.StreetAddress;
+            orderHeaderFromDB.City = orderVM.OrderHeader.City;
+            orderHeaderFromDB.State = orderVM.OrderHeader.State;
+            orderHeaderFromDB.PostalCode = orderVM.OrderHeader.PostalCode;
+            orderHeaderFromDB.ShipmentDate=orderVM.OrderHeader.ShipmentDate;
+
+            if (!string.IsNullOrEmpty(orderVM.OrderHeader.Carrier))
+                orderHeaderFromDB.Carrier = orderVM.OrderHeader.Carrier;
+
+            if (!string.IsNullOrEmpty(orderVM.OrderHeader.TrackingNumber))
+                orderHeaderFromDB.TrackingNumber = orderVM.OrderHeader.TrackingNumber;
+
+            _unitOfWork.OrderHeader.Update(orderHeaderFromDB);
+            _unitOfWork.Save();
+
+            TempData["Success"] = "Order Details Updated Successfully.";
+
+            return RedirectToAction(nameof(Details), new { id = orderHeaderFromDB.Id });
+        }
+
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult StartProcessing(OrderVM orderVM)
+        {
+            var orderHeaderFromDB = _unitOfWork.OrderHeader
+                .Get(x => x.Id == orderVM.OrderHeader.Id);
+            if (orderHeaderFromDB == null)
+                return NotFound();
+            orderHeaderFromDB.OrderStatus = SD.StatusInProcess;
+            _unitOfWork.OrderHeader.Update(orderHeaderFromDB);
+            _unitOfWork.Save();
+            TempData["Success"] = "Order Details Updated Successfully.";
+
+            return RedirectToAction(nameof(Details), new { id = orderVM.OrderHeader.Id });
+        }
+
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        public IActionResult CancelOrder(OrderVM orderVM)
+        {
+            var orderHeaderFromDB = _unitOfWork.OrderHeader
+                .Get(x => x.Id == orderVM.OrderHeader.Id);
+            if (orderHeaderFromDB == null)
+                return NotFound();
+            orderHeaderFromDB.OrderStatus = SD.StatusInProcess;
+            _unitOfWork.OrderHeader.Remove(orderHeaderFromDB);
+            _unitOfWork.Save();
+            TempData["Success"] = "Order Details Removed Successfully.";
+
+            return RedirectToAction(nameof(Details));
         }
 
         [HttpGet]
         public IActionResult GetAll(string status)
         {
-            IEnumerable<OrderHeader> orderHeaderObj = _unitOfWork.OrderHeader.GetAll(includeProperty: "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> orderHeaderObj; 
+            if(User.IsInRole(SD.Role_Admin)|| User.IsInRole(SD.Role_Employee))
+            {
+                orderHeaderObj = _unitOfWork.OrderHeader.GetAll(includeProperty: "ApplicationUser").ToList();
+            }
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                orderHeaderObj = _unitOfWork.OrderHeader.GetAll(x => x.ApplicationUserId== userId, includeProperty: "ApplicationUser");
+            }
 
             switch (status)
             {
